191a9ea8dc392ce0f5928908fda4ac9c6113d9f7
Seungyeoljeon <contact@patentwith.com>
2024-02-26 18:18:12 +0900

    공통모듈분리

--- js/main.js
+++ js/main.js
@@ -5,19 +5,6 @@ img.onload = function() {
   console.log('Image is loaded');
 };
 
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-searchEl.addEventListener('click', function() {
-  searchInputEl.focus();
-});
-searchInputEl.addEventListener('focus', function() {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-searchInputEl.addEventListener('blur', function() {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
 
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
@@ -159,7 +146,3 @@ spyEls.forEach(function (spyEl) {
 
 
 });
-
-
-const thisYear = document.querySelector('.this-year'); //class 선택자 . 붙여야함
-thisYear.textContent = new Date().getFullYear();//this year 2024
